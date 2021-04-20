942b138aaf5a7606b224d1f048f2f45a5974e173
Iurii <yuriyb83@gmail.comm>
2021-04-20 19:38:43 -0400

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ name, email, id }) => {
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h3>{name}</h3>
+        <h2>{name}</h2>
         <p>{email}</p>
       </div>
     </div>
